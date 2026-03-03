c43d0415f273717b8a10e8ffed92bd0f2bfcfa4f
Enissahin <enisSahin@gmail.com>
2026-03-03 23:03:57 +0300

    vs code ile commit

---
+++ 3.Çıktı Verme/app.js
@@ -0,0 +1,30 @@
+// Document write() ile çıktı verebilriiz
+
+// document objesine nokta koyduğumuzda içinde birçok metot,değişken vs var.
+// document.writeln("Hello world<br>");
+// document.writeln("Enis Şahin <br>");
+// document.writeln(55 + "<br>");
+// document.writeln(true);
+
+
+// console.log() metodunu kullanırız
+
+// console.log("Hello World");
+// console.log(5.5);
+// console.log(true);
+// let a =5;
+// let b=3;
+// console.log("a ve b'nin toplamı: ", a+b);
+// console.log("a ve b'nin toplamı "  + (a+b));
+ 
+// alert(uyarı) popup ile çıktı verme işlemleri
+
+// alert("Merhabalar ");
+// let a=10;
+// let b=20;
+// alert("iki sayının toplamı : " + (a+b));
+// alert("Lütfen F5 tuşuna basmayınız. ");
+
+console.log(window);
+console.log((window.location.port));
+console.error("hata!!");
